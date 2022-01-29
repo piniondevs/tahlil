764d819b7cd90b94106a7cbfc0166a3879409b95
piniondevs <sucmanuts420@gmail.com>
2022-01-29 10:48:25 +0000

    well this works somehow

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,21 @@ import React from "react";
 import axios from "axios";
 import Link from "next/link";
 
-export async function getStaticProps() {
-  const res = await axios.get(
-    "https://tahlils-site.vercel.app/api/posts"
-  );
-  const { data } = res;
-  return {
-    props: {
-      data,
-    },
-  };
-}
+export default function Home() {
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-export default function Home({ data }) {
+  React.useEffect(() => {
+    axios
+      .get("/api/posts")
+      .then((res) => {
+        setData(res.data);
+        console.log("blog posts loaded");
+        setLoading(false);
+      })
+      .catch(console.error);
+  }, []);
+
   const socials = [
     { name: "Email", url: "mailto:realtahlil@gmail.com?Subject=Hello" },
     { name: "GitHub", url: "https://github.com/tahlilma" },
@@ -25,34 +27,38 @@ export default function Home({ data }) {
     { name: "Instagram", url: "https://www.instagram.com/ta_hlil/" },
   ];
 
-  return (
-    <div>
-      <h1>Hi</h1>
-      <p>
-        My names <strong>Tahlil</strong>, Im a self proclaimed software
-        developer and I mainly work on mobile apps, websites and Discord bots.
-        I also do other stuff from time to time.
-      </p>
+  if (loading) {
+    return <p>loading</p>;
+  } else {
+    return (
+      <div>
+        <h1>Hi</h1>
+        <p>
+          My names <strong>Tahlil</strong>, Im a self proclaimed software
+          developer and I mainly work on mobile apps, websites and Discord bots.
+          I also do other stuff from time to time.
+        </p>
 
-      <h2>Contact Me:</h2>
-      {socials.map((item) => {
-        return (
-          <div className="link-container">
-            <a href={item.url}>{item.name}</a>
-          </div>
-        );
-      })}
+        <h2>Contact Me:</h2>
+        {socials.map((item) => {
+          return (
+            <div className="link-container">
+              <a href={item.url}>{item.name}</a>
+            </div>
+          );
+        })}
 
-      <h2>Blog Posts:</h2>
-      {data.payload.map((item) => {
-        return (
-          <div className="link-container">
-            <Link href={`/${encodeURIComponent(item)}`}>
-              <a>→ {item}</a>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
-  );
+        <h2>Blog Posts:</h2>
+        {data.payload.map((item) => {
+          return (
+            <div className="link-container">
+              <Link href={`/blog/${encodeURIComponent(item)}`}>
+                <a>→ {item}</a>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
